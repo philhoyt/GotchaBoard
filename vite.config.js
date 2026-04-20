e78b55a73345b62bd6a368227c0e8eec41ce6875
Phil Hoyt <philhoyt@users.noreply.github.com>
2026-04-20 08:56:32 -0400

    Show app version in settings panel footer

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite';
 import { resolve }      from 'path';
+import { readFileSync } from 'fs';
+
+const { version } = JSON.parse(readFileSync('./package.json', 'utf-8'));
 
 export default defineConfig({
+  define: {
+    __APP_VERSION__: JSON.stringify(version),
+  },
   root: 'src',
 
   build: {
